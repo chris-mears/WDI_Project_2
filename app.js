38dd99fd295211bae2f5e759e649e707ef136cfd
SadEvilPenguin <mears.chris@gmail.com>
2017-09-28 13:50:55 -0400

    update app.js to get local host to work

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err);
+})
+
+db.once('open', () => {
+    console.log("Connected to Mongodb")
+})
 
 const index = require('./routes/index');
 const users = require('./routes/users');
